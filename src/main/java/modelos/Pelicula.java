816159b915d2fc1b10cabba76c748b13d0d3a839
Juan David <judavidpm@gmail.com>
2025-09-01 08:23:50 -0500

    Primer commit

---
+++ src/main/java/modelos/Pelicula.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelos;
+
+import javax.swing.ImageIcon;
+
+/**
+ *
+ * @author Nimac
+ */
+public class Pelicula {
+    
+}
